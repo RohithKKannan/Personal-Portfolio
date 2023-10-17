598b394674e1541b2116dcae31db8d09df7d9b3e
RohithKannan <51419779+RohithKKannan@users.noreply.github.com>
2023-10-17 20:17:12 +0530

    Added a class to div for social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import "../styles/Footer.css"
 function Footer() {
     return (
         <div className='footer'>
-            <div>
+            <div className='socialMedia'>
                 <LinkedInIcon />
                 <YoutubeIcon />
                 <GithubIcon />
