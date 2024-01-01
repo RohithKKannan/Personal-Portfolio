b7684ab6311f695dfc6a41971fd1df4c10daaa77
RohithKKannan <hithu203@gmail.com>
2024-01-01 15:35:33 +0530

    Projects page initial setup

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,23 @@
+import BallinImage from '../assets/ballinImage.png'
+import MetalMayhemImage from '../assets/metalMayhemImage.png'
+import BlockBusterImage from '../assets/blockBusterImage.png'
+import ExplorerImage from '../assets/explorerImage.png'
+
+export const projectList = [
+    {
+        name: "Ballin'",
+        image: BallinImage
+    },
+    {
+        name: "Metal Mayhem",
+        image: MetalMayhemImage
+    },
+    {
+        name: "Block Buster",
+        image: BlockBusterImage
+    },
+    {
+        name: "The Explorer",
+        image: ExplorerImage
+    }
+]
